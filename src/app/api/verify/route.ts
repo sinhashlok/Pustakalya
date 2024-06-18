08cf00d248da40b97c989215468530dd914c835e
sinhashlok <shlokjp@gmail.com>
2024-06-18 23:34:45 +0530

    console.logs for verify API

--- src/app/api/verify/route.ts
+++ src/app/api/verify/route.ts
@@ -5,14 +5,17 @@ import prisma from "@/db";
 export async function POST(req: NextRequest) {
   try {
     // Authorization Token
+    console.log(req.headers);
+
     const bearerToken = req.headers.get("authorization") || "";
     const token = bearerToken.split(" ").at(1) || "";
     const payload = await verifyJwtToken(token);
     const userId = payload?.payload?.userId || "";
 
     const body = await req.json();
-    const { code }: { code: string } = body
-    
+    console.log(body);
+
+    const { code }: { code: string } = body;
 
     const user = await prisma.user.findUnique({
       where: {
