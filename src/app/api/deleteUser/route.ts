f9eebed2e0a5d52435d1a932e52dad5c65a9abd0
sinhashlok <shlokjp@gmail.com>
2024-07-28 22:36:41 +0530

    Redeploy

--- src/app/api/deleteUser/route.ts
+++ src/app/api/deleteUser/route.ts
@@ -10,7 +10,7 @@ export async function POST(req: NextRequest) {
     if (!userId) {
       return NextResponse.json(
         { message: "Unauthenticated", success: false },
-        { status: 401 },
+        { status: 401 }
       );
     }
 
@@ -20,12 +20,12 @@ export async function POST(req: NextRequest) {
 
     return NextResponse.json(
       { message: "Account deleted", success: true },
-      { status: 200 },
+      { status: 200 }
     );
   } catch (error: any) {
     return NextResponse.json(
       { error: error.message, success: false },
-      { status: 500 },
+      { status: 500 }
     );
   }
 }
